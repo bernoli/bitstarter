1cae1ddabf76604cc56f0ec7e355c9bfd0f41d8d
Yoav Kaplan <yoav.kaplan@gmail.com>
2013-07-09 21:19:34 +0000

    Grader support url using restler

--- grader.js
+++ grader.js
@@ -42,13 +42,44 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks &lt;check_file&gt;', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file &lt;html_file&gt;', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u,--url <url_file>', 'Path to url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+  
+	if (program.url.length > 0)
+	{
+	   getUrlHtml(program.url);
+	}
+ 	else{
+ 	var checkJson = checkHtmlFile(program.file, program.checks);
+
+     	var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);	
+        }
+} 
+else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
+function getUrlHtml($url)
+{
+
+	var rest = require('restler');  
+        rest.get($url).on('complete', function (result){
+	if (result instanceof Error)
+	{
+	     console.log('Error: ' + result.message);
+	} else
+	{
+		fs.writeFile('temp.html', result);
+       		console.log('program checks = ' + program.checks);
+  		var checkJson = checkHtmlFile('temp.html', program.checks);	
+     		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	}
+
+
+	})
+};
 
