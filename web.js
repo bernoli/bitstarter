0a8f895c87f8f652210cfa1667b6043cc24c58c3
Yoav Kaplan <yoav.kaplan@gmail.com>
2013-07-09 19:04:15 +0000

    Add index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var buf = require('buffer');
 
+var inputFile = 'index.html';
+
+
+var inputBuffer = fs.readFileSync(inputFile);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(inputBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
